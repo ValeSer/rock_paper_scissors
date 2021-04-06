2f7a854fe1086c4268ffaea1f896eb886829c4e3
Valeria Serci <balerty84@gmail.com>
2021-04-06 12:34:29 +0100

    modify input to lowercase

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const rock = 'rock';
 const paper = 'paper';
 const scissors = 'scissors';
 
-const getUserChoice = userInput => {};
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+};
+
